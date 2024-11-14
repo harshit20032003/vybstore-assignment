de1b51fb4297903a795dece5d77107bfb42937f6
choccy2 <kujoxd6@gmail.com>
2024-11-15 05:14:40 +0530

    1.3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  eslint: {
+    ignoreDuringBuilds: true, // This will ignore ESLint errors during the build process
+  },
 };
 
 export default nextConfig;
